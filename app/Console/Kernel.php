075e29b336c8c4d80978dab30ba163d17e2492ae
Yves NJAMEN <njamenconsulting@gmail.com>
2022-09-06 11:47:49 +0200

    heroku configuration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Controllers\PlentymarketController;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +17,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            $pc = (new PlentymarketController())-> updatePriceAndStock();
+           // $pc->updatePriceAndStock();
+        })->everyMinute();//->hourly();//
     }
 
     /**
